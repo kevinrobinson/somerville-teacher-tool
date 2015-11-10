d7db131d3519d8414057c471c5465a953c675f04
Kevin Robinson <kevin.robinson.0@gmail.com>
2015-11-10 18:22:01 -0500

    Try removing lodash for now

--- webpack/webpack.config.development.js
+++ webpack/webpack.config.development.js
@@ -1,9 +1,18 @@
 var path = require('path');
-var _ = require('lodash');
 var sharedConfig = require('./webpack.config.shared.js');
 
+var merge = function(first, second) {
+  var merged = {}
+  Object.keys(first).forEach(function(key) {
+    merged[key] = first[key];
+  });
+  Object.keys(second).forEach(function(key) {
+    merged[key] = second[key];
+  });
+  return merged;
+};
 
-module.exports = _.merge(sharedConfig, {
+module.exports = merge(sharedConfig, {
   output: {
     path: path.resolve(__dirname, 'dist_dev'),
     publicPath: '/javascripts',
