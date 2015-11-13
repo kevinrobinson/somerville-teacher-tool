e7e5d5fc91d539c6f82a856d5a7fe4b76d337901
Kevin Robinson <kevin.robinson.0@gmail.com>
2015-11-13 01:29:42 -0500

    Add comments to Rails build script

--- scripts/rails_build.sh
+++ scripts/rails_build.sh
@@ -4,27 +4,31 @@
 # Any tests should have run and passed at this point.
 # Need docker hub credentials to have been set beforehand.
 
-# Clear any previously built assets from dev or prod builds.
+echo "Cleaning previously built assets from dev or prod builds..."
 scripts/rails_clear.sh
 
-# Build webpack container to perform the build, use it to build
-# the assets for production.
+echo "Building a webpack container to perform the build..."
 docker build -t webpack-building-production webpack/
+
+echo "Building production assets..."
 docker run \
   -v ~/github/somerville-teacher-tool/webpack/src:/mnt/webpack/src \
   -v ~/github/somerville-teacher-tool/webpack/dist_production:/mnt/webpack/dist_production \
   webpack-building-production \
   npm run build:production
 
-# Copy the manifest into the Rails folder, so the Rails HTML frame can point to the main JS file.
-# And put the assets in S3, where the Cloudfront CDN will read them from.
+echo "Copying manifest to Rails and copying assets to S3..."
 MANIFEST_FILE=webpack-assets.json
 cp -r ~/github/somerville-teacher-tool/webpack/dist_production/$MANIFEST_FILE ~/github/somerville-teacher-tool/public/js/$MANIFEST_FILE
 aws s3 cp ./webpack/dist_production s3://somerville-teaching-tool-cdn/production/js --exclude "$MANIFEST_FILE" --recursive
 
-# Build the production Rails image and push it.
+echo "Building the production Rails image..."
 docker build -t kevinrobinson/somerville-teaching-tool:production_rails .
+
+echo "Pushing the production Rails image to the Docker registry..."
 docker push kevinrobinson/somerville-teaching-tool:production_rails
 
-# Clear any assets we generated in the process.
+echo "Clearing any assets we generated in the process..."
 scripts/rails_clear.sh
+
+echo "Done."
