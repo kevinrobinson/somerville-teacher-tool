5e30d83c13a5d5310206fd3a83f28db4d60fee8a
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-01-14 15:35:42 -0500

    Fix bug with overview

--- app/assets/javascripts/school/overview_page.js
+++ app/assets/javascripts/school/overview_page.js
@@ -149,7 +149,7 @@ $(function() {
           style: { fontSize: styles.fontSize }
         },
           dom.div({ className: 'header', style: styles.header }, createEl(SlicePanels, {
-            allStudents: this.props.students,
+            allStudents: this.props.allStudents,
             students: this.filteredStudents(),
             InterventionTypes: this.props.InterventionTypes,
             filters: this.state.filters,
